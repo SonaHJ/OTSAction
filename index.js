b9857712896476510bf4e488662e6a1c9a791465
SonaHJ <64142721+SonaHJ@users.noreply.github.com>
2022-07-21 14:25:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -350,6 +350,9 @@ async function validateAssetId(serverStore, asset) {
           response.data
         );
       }
+	  console.log("response"+ response);
+	   console.log("response.data"+ response.data);
+	   console.log("response.data.totalElements"+ response.data.totalElements);
       var parsedJSON = response.data;
       var total = parsedJSON.totalElements;
       var retrievedAssetId;
